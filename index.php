f607a2e90c8ef55480ccbe69e5830e6af2ea0b04
Alex Druk <alex.druk@gmail.com>
2020-08-13 18:29:00 +0200

    home page

--- index.php
+++ index.php
@@ -3,8 +3,8 @@ require_once __DIR__.'/globals.php';
 $template_info["ERROR"] = $ERROR;
 $template_info["page_description"] = 'Антология современной и старинной китайской поэзии. Свыше 3000 стихов 429 авторов от VI в. до н.э. вплоть до наших дней в лучших переводах.';
 $template_info["title"] ='Поэзия Китая';
-$template_info["content"] ='Chinese poetry web portal project';
+#$template_info["content"] ='Chinese poetry web portal project';
 
-$template = $twig->load('page_root.html.twig');
+$template = $twig->load('home.html.twig');
 echo $template->display($template_info);
 ?>
