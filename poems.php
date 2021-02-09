ea4bd35c2a74bdd51b08f994dff0dfcc582bd6ab
Alex Druk <alex.druk@gmail.com>
2021-02-09 17:47:11 +0100

    fixed cache.

--- poems.php
+++ poems.php
@@ -101,8 +101,14 @@ elseif  ( ($_GET['action'] == 'bySource') && (array_key_exists('biblio_id', $_GE
 }
 elseif ($_GET['action'] == 'showrandom') {
     $poem_id = getRandomPoemID();
-    header("Location: poems.php?action=show&poem_id=$poem_id");
-}
+    $loc = "Location: poems.php?action=show&poem_id=".$poem_id."&r=".mt_rand(0, 9999999);
+    header($loc);
+    header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+    header("Pragma: no-cache"); // HTTP/1.0
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    }
 elseif ($_GET['action'] == 'search') {
     $template_info["header"] ='Полнотекстный поиск по переводам стихов';
     if (array_key_exists('posted', $_GET)) {
