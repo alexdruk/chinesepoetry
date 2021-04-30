81997f87335700edc336de6baf978cd115316604
Alex Druk <alex.druk@gmail.com>
2021-04-30 13:01:26 +0200

    added 'selected'

--- functions.php
+++ functions.php
@@ -1449,6 +1449,34 @@ function getRandomPoemID() {
 	}
 }
 /**
+ * get a random  poem from selectedpoems
+ *
+ * @return array random poem_id
+ * @throws DBException
+ */
+function getRandomSelectedPoemID() {
+	$db = UserConfig::getDB();
+	$records = array();
+	$record = null;
+	if ($stmt = $db->prepare('SELECT `poems_id` FROM  selectedpoems ORDER BY RAND() LIMIT 1;')) {
+		if (!$stmt->execute()) {
+			throw new DBExecuteStmtException($db, $stmt);
+		}
+		if (!$stmt->bind_result($poems_id)) {
+			throw new DBBindResultException($db, $stmt);
+		}
+		while ($stmt->fetch() === TRUE) {
+			$record = $poems_id;
+		}
+		$stmt->free_result();
+		$stmt->close();
+		return $record;
+	} else {
+		throw new DBPrepareStmtException($db);
+	}
+}
+
+/**
  * get a list of all records from table authors by topic
  *
  * @return array array of records
--- poems.php
+++ poems.php
@@ -109,6 +109,17 @@ elseif ($_GET['action'] == 'showrandom') {
     header("Pragma: no-cache"); // HTTP/1.0
     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
     }
+elseif ($_GET['action'] == 'selected') {
+    $poem_id = getRandomSelectedPoemID();
+    $loc = "Location: poems.php?action=show&poem_id=".$poem_id;
+    header($loc);
+    header("Cache-Control: no-store, no-cache, must-revalidate"); // HTTP/1.1
+    header("Cache-Control: post-check=0, pre-check=0", false);
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
+    header("Pragma: no-cache"); // HTTP/1.0
+    header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
+    }
+    
 elseif ($_GET['action'] == 'search') {
     $template_info["header"] ='Полнотекстный поиск по переводам стихов';
     if (array_key_exists('posted', $_GET)) {
