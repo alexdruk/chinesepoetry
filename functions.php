8c8f074f135957679e06bc9ffc7f330c6b65cede
Alex Druk <alexdruk@gmail.com>
2023-03-27 17:08:13 +0200

    fixed a bug introduced by sex

--- functions.php
+++ functions.php
@@ -2929,10 +2929,11 @@ function makeTranslator($translator1_id, $translator2_id) {
     return $translator;    
 }
 function makeAuthor($author_id){
-    list($author_id, $full_name, $proper_name,  $dates,  $epoch, $present, $zh_trad, $zh_simple) = getByIDFromAuthors($author_id);
+	list($author_id, $full_name, $proper_name,  $dates,  $epoch, $present, $sex, $zh_trad, $zh_simple) = getByIDFromAuthors($author_id);
     $author = '<a href="./authors.php?action=show&record_id='.$author_id.'"><span class="author name">'.$proper_name.'</span>
     &nbsp;<span class="author dates">'.$dates.'</span></a>';
-    if ($zh_trad) {
+	echo 'zh_trad = ' . $zh_trad . 'zh_simple=' . $zh_simple . '<br>';
+	if ($zh_trad) {
         $author .= '&nbsp;<span class="name zh">'.$zh_trad.'</span>';
     }
     else if ($zh_simple) {
